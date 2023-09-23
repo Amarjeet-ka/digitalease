995dba346cc02d11b12ab0c922bfa9c8d59f3967
Amarjeet-ka <116912077+Amarjeet-ka@users.noreply.github.com>
2023-09-23 11:08:45 +0530

    added navigation to landing page

---
+++ src/components/NavigationOuter.js
@@ -0,0 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import d from "../assets/dee.jpeg"
+
+function NavigationOuter() {
+  return (
+    <>
+      <Navbar style={{ backgroundColor: '#f7f7f7' }} data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home" style={{ color: 'black', fontSize: '18px' }}>
+          <img src={d}  height={120}  alt='image not found' />
+
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home" style={{ color: 'black', fontSize: '16px' }}>Home</Nav.Link>
+            <Nav.Link href="#features" style={{ color: 'black', fontSize: '16px' }}>Features</Nav.Link>
+            <Nav.Link href="#pricing" style={{ color: 'black', fontSize: '16px' }}>Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavigationOuter;
